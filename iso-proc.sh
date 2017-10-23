55e0ddbfa37d16a8e0bbb35836abe56aa0018108
Dave D <davey@localhost.localdomain>
2017-10-23 16:47:16 -0500

    directory processor added

--- iso-proc.sh
+++ iso-proc.sh
@@ -1,7 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+for file in *.iso
+do
+        dir="${file%.iso}"
+        mkdir -- "$dir"
+        mv -- "$file" "$dir"
+done
 
 find "$PWD" -type d | sort | while read dir
-   do cd "${dir}"
-   [ ! -f md5sum.txt ] && echo "Processing " "${dir}" || echo "Skipping " "${dir}" " md5sum.txt already present" 
-   [ ! -f md5sum.txt ] &&  md5sum * > md5sum.txt ; chmod a=r "${dir}"/md5sum.txt 
-done 
+        do cd "${dir}"
+        [ ! -f md5sum.txt ] && echo "Processing " "${dir}" || echo "Skipping " "${dir}" " md5sum.txt already present" 
+        [ ! -f md5sum.txt ] &&  md5sum * > md5sum.txt
+        chmod a=r "${dir}"/md5sum.txt
+done
+
